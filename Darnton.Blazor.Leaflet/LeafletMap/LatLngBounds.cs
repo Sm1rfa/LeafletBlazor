55c19a2179153dd12a08fadc4b29c145465c200c
P33tr <pmarshall@simur.es>
2021-11-23 22:14:06 +0100

    corectly maps to the LatLngBounds from leaflet

--- Darnton.Blazor.Leaflet/LeafletMap/LatLngBounds.cs
+++ Darnton.Blazor.Leaflet/LeafletMap/LatLngBounds.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.JSInterop;
+﻿using System.Collections.Generic;
+using Microsoft.JSInterop;
 using System.Threading.Tasks;
 
 namespace Blazor.Leaflet.OpenStreetMap.LeafletMap
@@ -9,24 +10,28 @@ namespace Blazor.Leaflet.OpenStreetMap.LeafletMap
     /// </summary>
     public class LatLngBounds : InteropObject
     {
-        private LatLng _corner1;
-        private LatLng _corner2;
-
-        /// <summary>
-        /// Constructs a LatLngBounds instance.
-        /// </summary>
-        /// <param name="corner1">The first corner defining the bounds.</param>
-        /// <param name="corner2">The second corner defining the bounds, diagonally opposite the first.</param>
-        public LatLngBounds(LatLng corner1, LatLng corner2)
+        public LatLngBounds()
         {
-            _corner1 = corner1;
-            _corner2 = corner2;
+        }
+
+        public LatLngBounds(LatLng southwest, LatLng northeast)
+        {
+            _southWest = southwest;
+            _northEast = northeast;
+        }
+
+        public LatLng _southWest { get; set; }
+        public LatLng _northEast { get; set; }
+
+        public IEnumerable<LatLng> ToLatLng()
+        {
+            return new List<LatLng>() { _southWest, _northEast };
         }
 
         /// <inheritdoc/>
         protected override async Task<IJSObjectReference> CreateJsObjectRef()
         {
-            return await JSBinder.JSRuntime.InvokeAsync<IJSObjectReference>("L.latLngBounds", _corner1.JSObjectReference, _corner2.JSObjectReference);
+            return await JSBinder.JSRuntime.InvokeAsync<IJSObjectReference>("L.latLngBounds", _southWest.JSObjectReference, _northEast.JSObjectReference);
         }
     }
 }
